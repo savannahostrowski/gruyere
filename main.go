4a3ca86a9ec6194972d3bfa2f25343c271694efe
Savannah Ostrowski <savannahostrowski@gmail.com>
2023-02-19 16:21:40 -0800

    got processes

---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/table"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var baseStyle = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("240"))
+
+type model struct {
+	table table.Model
+}
+
+type proc struct {
+	pid  string
+	user string
+	port string
+	comm string
+}
+
+func (m model) Init() tea.Cmd { return nil }
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "esc":
+			if m.table.Focused() {
+				m.table.Blur()
+			} else {
+				m.table.Focus()
+			}
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		case "enter":
+			return m, tea.Batch(
+				tea.Printf("Let's go to %s!", m.table.SelectedRow()[1]),
+			)
+		}
+	}
+	m.table, cmd = m.table.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return baseStyle.Render(m.table.View()) + "\n"
+}
+
+func main() {
+	cmd := "lsof -i -P -n | grep LISTEN | awk '{print $2, $3, $9, $1}'"
+	out, err := exec.Command("bash", "-c", cmd).Output()
+	str_stdout := string(out)
+
+	procs := strings.Split(str_stdout, "\n")
+	var rows []table.Row
+	for _, proc := range procs {
+		rows = append(rows, strings.Fields(proc))
+	}
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	columns := []table.Column{
+		{Title: "PID", Width: 10},
+		{Title: "User", Width: 10},
+		{Title: "Port", Width: 20},
+		{Title: "Command", Width: 10},
+	}
+
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused(true),
+		table.WithHeight(7),
+	)
+
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(false)
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("57")).
+		Bold(false)
+	t.SetStyles(s)
+
+	m := model{t}
+	if _, err := tea.NewProgram(m).Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+search for port
+select port to terminate process -> kill command under the hood (make sure to add confirmation)
