578d6ca76f8c325793ffa9dfbe0d9f5b4cbbad84
Savannah Ostrowski <savannahostrowski@gmail.com>
2023-02-19 17:27:28 -0800

    added title

--- main.go
+++ main.go
@@ -9,17 +9,43 @@ import (
 	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/lucasb-eyer/go-colorful"
 )
 
 var baseStyle = lipgloss.NewStyle().
 	BorderStyle(lipgloss.NormalBorder()).
 	BorderForeground(lipgloss.Color("240"))
 
+var (
+	subtle     = lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#383838"}
+	titleStyle = lipgloss.NewStyle().
+			MarginLeft(1).
+			MarginRight(5).
+			Padding(0, 1).
+			Italic(true).
+			Foreground(lipgloss.Color("#FFF7DB")).
+			SetString("Gruyere")
+
+	descStyle = lipgloss.NewStyle().MarginTop(1)
+
+	infoStyle = lipgloss.NewStyle().
+			BorderStyle(lipgloss.NormalBorder()).
+			BorderTop(true).
+			BorderForeground(subtle)
+	docStyle = lipgloss.NewStyle().Padding(1, 2, 1, 2)
+)
+
 type model struct {
-	table table.Model
+	table        table.Model
+	selectedPort string
 }
 
-func (m model) Init() tea.Cmd { return nil }
+var doc = strings.Builder{}
+
+func (m model) Init() tea.Cmd {
+	renderTitle()
+	return nil
+}
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
@@ -35,8 +61,9 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "q", "ctrl+c":
 			return m, tea.Quit
 		case "enter":
+			m.selectedPort = m.table.SelectedRow()[0]
 			return m, tea.Batch(
-				tea.Printf("Let's go to %s!", m.table.SelectedRow()[1]),
+				tea.Printf(m.table.SelectedRow()[0]),
 			)
 		}
 	}
@@ -55,7 +82,7 @@ func main() {
 	procs := strings.Split(str_stdout, "\n")
 	var rows []table.Row
 	for i, proc := range procs {
-		if len(proc) == 0 || i == 0{ 
+		if len(proc) == 0 || i == 0 {
 			continue
 		}
 		pieces := strings.Fields(proc)
@@ -97,9 +124,62 @@ func main() {
 		Bold(false)
 	t.SetStyles(s)
 
-	m := model{t}
+	m := model{t, ""}
 	if _, err := tea.NewProgram(m).Run(); err != nil {
 		fmt.Println("Error running program:", err)
 		os.Exit(1)
 	}
+}
+
+func renderTitle() {
+	var (
+		colors = colorGrid(1, 5)
+		title  strings.Builder
+	)
+
+	for i, v := range colors {
+		const offset = 2
+		c := lipgloss.Color(v[0])
+		fmt.Fprint(&title, titleStyle.Copy().MarginLeft(i*offset).Background(c))
+		if i < len(colors)-1 {
+			title.WriteRune('\n')
+		}
+	}
+
+	desc := lipgloss.JoinVertical(lipgloss.Left,
+		descStyle.Render("A tiny program for viewing + killing ports"),
+		infoStyle.Render("Here's what's running..."),
+	)
+
+	row := lipgloss.JoinHorizontal(lipgloss.Top, title.String(), desc)
+	doc.WriteString(row + "\n\n")
+	fmt.Println(docStyle.Render(doc.String()))
+}
+
+func colorGrid(xSteps, ySteps int) [][]string {
+	x0y0, _ := colorful.Hex("#F25D94")
+	x1y0, _ := colorful.Hex("#EDFF82")
+	x0y1, _ := colorful.Hex("#643AFF")
+	x1y1, _ := colorful.Hex("#14F9D5")
+
+	x0 := make([]colorful.Color, ySteps)
+	for i := range x0 {
+		x0[i] = x0y0.BlendLuv(x0y1, float64(i)/float64(ySteps))
+	}
+
+	x1 := make([]colorful.Color, ySteps)
+	for i := range x1 {
+		x1[i] = x1y0.BlendLuv(x1y1, float64(i)/float64(ySteps))
+	}
+
+	grid := make([][]string, ySteps)
+	for x := 0; x < ySteps; x++ {
+		y0 := x0[x]
+		grid[x] = make([]string, xSteps)
+		for y := 0; y < xSteps; y++ {
+			grid[x][y] = y0.BlendLuv(x1[x], float64(y)/float64(xSteps)).Hex()
+		}
+	}
+
+	return grid
 }
